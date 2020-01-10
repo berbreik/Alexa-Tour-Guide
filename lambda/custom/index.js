f034343b444d871d9642c8ad03b73e8f5df24189
schenne <sylvesterchenne@gmail.com>
2020-01-09 19:17:56 -0800

    Initial commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,509 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+/* eslint-disable  no-use-before-define */
+
+
+// City Guide: A sample Alexa Skill Lambda function
+//  This function shows how you can manage data in objects and arrays,
+//   choose a random recommendation,
+//   call an external API and speak the result,
+//   handle YES/NO intents with session attributes,
+//   and return text data on a card.
+
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+const https = require('https');
+
+
+// 1. Handlers ===================================================================================
+
+const LaunchHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechOutput = `${requestAttributes.t('WELCOME')} ${requestAttributes.t('HELP')}`;
+        return responseBuilder
+            .speak(speechOutput)
+            .reprompt(speechOutput)
+            .getResponse();
+    },
+};
+
+const AboutHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AboutIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+
+        return responseBuilder
+            .speak(requestAttributes.t('ABOUT'))
+            .getResponse();
+    },
+};
+
+const CoffeeHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'CoffeeIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const restaurant = randomArrayElement(getRestaurantsByMeal('coffee'));
+        sessionAttributes.restaurant = restaurant.name;
+        const speechOutput = `For a great coffee shop, I recommend, ${restaurant.name}. Would you like to hear more?`;
+
+        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
+    },
+};
+
+const BreakfastHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'BreakfastIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const restaurant = randomArrayElement(getRestaurantsByMeal('breakfast'));
+        sessionAttributes.restaurant = restaurant.name;
+        const speechOutput = `For breakfast, try this, ${restaurant.name}. Would you like to hear more?`;
+
+        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
+    },
+};
+
+const LunchHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'LunchIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const restaurant = randomArrayElement(getRestaurantsByMeal('lunch'));
+        sessionAttributes.restaurant = restaurant.name;
+        const speechOutput = `Lunch time! Here is a good spot. ${restaurant.name}. Would you like to hear more?`;
+
+        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
+    },
+};
+
+const DinnerHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'DinnerIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const restaurant = randomArrayElement(getRestaurantsByMeal('dinner'));
+        sessionAttributes.restaurant = restaurant.name;
+        const speechOutput = `Enjoy dinner at, ${restaurant.name}. Would you like to hear more?`;
+
+        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
+    },
+};
+
+const YesHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.YesIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const restaurantName = sessionAttributes.restaurant;
+        const restaurantDetails = getRestaurantByName(restaurantName);
+        const speechOutput = `${restaurantDetails.name
+        } is located at ${restaurantDetails.address
+        }, the phone number is ${restaurantDetails.phone
+        }, and the description is, ${restaurantDetails.description
+        }  I have sent these details to the Alexa App on your phone.  Enjoy your meal!
+        <say-as interpret-as="interjection">bon appetit</say-as>`;
+
+        const card = `${restaurantDetails.name}\n${restaurantDetails.address}\n$
+        {data.city}, ${data.state} ${data.postcode}\nphone: ${restaurantDetails.phone}\n`;
+
+        return responseBuilder
+            .speak(speechOutput)
+            .withSimpleCard(SKILL_NAME, card)
+            .getResponse();
+    },
+};
+
+const AttractionHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AttractionIntent';
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let distance = 200;
+        if (request.intent.slots.distance.value && request.intent.slots.distance.value !== "?") {
+            distance = request.intent.slots.distance.value;
+        }
+
+        const attraction = randomArrayElement(data.attractions);
+
+        const speechOutput = `Try ${
+            attraction.name}, which is ${
+            attraction.distance === '0' ? 'right downtown. ' : `${attraction.distance} miles away. Have fun! `
+        }${attraction.description}`;
+
+        return responseBuilder.speak(speechOutput).getResponse();
+    },
+};
+
+const GoOutHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'GoOutIntent';
+    },
+    handle(handlerInput) {
+        return new Promise((resolve) => {
+            getWeather((localTime, currentTemp, currentCondition) => {
+                const speechOutput = `It is ${localTime
+                } and the weather in ${data.city
+                } is ${
+                    currentTemp} and ${currentCondition}`;
+                resolve(handlerInput.responseBuilder.speak(speechOutput).getResponse());
+            });
+        });
+    },
+};
+
+const HelpHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('HELP'))
+            .reprompt(requestAttributes.t('HELP'))
+            .getResponse();
+    },
+};
+
+const StopHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.NoIntent'
+            || request.intent.name === 'AMAZON.CancelIntent'
+            || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('STOP'))
+            .getResponse();
+    },
+};
+
+const SessionEndedHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const request = handlerInput.requestEnvelope.request;
+
+        console.log(`Error handled: ${error.message}`);
+        console.log(` Original request was ${JSON.stringify(request, null, 2)}\n`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+const FallbackHandler = {
+
+  // 2018-May-01: AMAZON.FallackIntent is only currently available in en-US locale.
+
+  //              This handler will not be triggered except in that locale, so it can be
+
+  //              safely deployed for any locale.
+
+  canHandle(handlerInput) {
+
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'IntentRequest'
+
+      && request.intent.name === 'AMAZON.FallbackIntent';
+
+  },
+
+  handle(handlerInput) {
+
+    return handlerInput.responseBuilder
+
+      .speak(FALLBACK_MESSAGE)
+
+      .reprompt(FALLBACK_REPROMPT)
+
+      .getResponse();
+
+  },
+
+};
+
+
+// 2. Constants ==================================================================================
+
+const languageStrings = {
+    en: {
+        translation: {
+            WELCOME: 'Welcome to the Vancouver City Guide!',
+            HELP: 'Say about, to hear more about the city, or say coffee, breakfast, lunch, or dinner, to hear local restaurant suggestions, or say recommend an attraction, or say, go outside. ',
+            ABOUT: 'Vancouver is a city on the West Coast of Canada.  A popular tourist destination, Vancouver offers beautiful in all four seasons of the  year.',
+            STOP: 'Okay, see you next time!',
+        },
+    },
+    // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
+};
+const data = {
+    city: 'Vancouver',
+    state: 'BC',
+    postcode: '01930',
+    restaurants: [
+        {
+            name: "Miku Vancouver",
+            address: '200 Granville St #70',
+            phone: '604-568-3900',
+            meals: 'lunch, dinner',
+            description: 'Situated along the Burrard Inlet next to the Vancouver landmark Canada Place, Miku Waterfront delivers gourmet aburi sushi cuisine paired with its well known, energetic ambience.',
+        },
+        {
+            name: 'Jam Cafe',
+            address: '556 Beatty Street',
+            phone: '778-379-1992',
+            meals: 'coffee, breakfast, lunch',
+            description: 'An all-day breakfast and lunch cafe inspired by home-styled cooking and recipes from the south.',
+        },
+        {
+            name: 'The Flying Pig',
+            address: '102 Water St',
+            phone: '604-559-7968',
+            meals: 'lunch, dinner',
+            description: 'The Flying Pig Group of Restaurants is a collection of culinary experiences that deliver authentic food made by genuine people.',
+        },
+        {
+            name: 'Phnom Penh',
+            address: '244 Georgia St',
+            phone: '604-734-8898',
+            meals: 'lunch, dinner',
+            description: 'A family-owned Vietnamese-Cambodian restaurant that will knock your socks off.',
+        },
+        {
+            name: 'Kokoro Tokyo Mazezoba',
+            address: '551 Seymour St',
+            phone: '604-559-8872',
+            meals: 'lunch, dinner',
+            description: 'Modern and contemporary Japanese eatery serving soupless ramen noodles with creative toppings.',
+        },
+        {
+            name: "Bean Around The World",
+            address: '2528 Main St, Vancouver, BC',
+            phone: '604-558-4887',
+            meals: 'coffee, breakfast, lunch',
+            description: 'Craft coffee shop in a modern storefront setting with a light fare menu located in the heart of Mount Pleasant',
+        },
+
+    ],
+    attractions: [
+        {
+            name: 'Stanley Park Seawall',
+            description: 'Gloucester has tour boats that depart twice daily from Rogers street at the harbor.  Try either the 7 Seas Whale Watch, or Captain Bill and Sons Whale Watch. ',
+            distance: '0',
+        },
+        {
+            name: 'Whistler Blackcomb Ski Resort',
+            description: 'Whistler is a town north of Vancouver, British Columbia, that\'s home to Whistler Blackcomb, one of the largest ski resorts in North America. Besides skiing and snowboarding, the area offers snowshoeing, tobogganing and ski jumping at the Olympic Park, a venue for the 2010 Vancouver Winter Olympics. The hub of Whistler is a compact, chalet-style pedestrian village at the base of Whistler and Blackcomb mountains.',
+            distance: '2',
+        },
+        {
+            name: 'Capilano Suspension Bridge',
+            description: 'A quaint New England town, Rockport is famous for rocky beaches, seaside parks, lobster fishing boats, and several art studios.',
+            distance: '4',
+        },
+        {
+            name: 'Fenway Park',
+            description: 'Home of the Boston Red Sox, Fenway park hosts baseball games From April until October, and is open for tours. ',
+            distance: '38',
+        },
+    ],
+};
+
+const SKILL_NAME = 'Vancouver City Guide';
+const FALLBACK_MESSAGE = `The ${SKILL_NAME} skill can\'t help you with that.  It can help you learn about Vancouver if you say tell me about this place. What can I help you with?`;
+const FALLBACK_REPROMPT = 'What can I help you with?';
+
+
+
+// 3. Helper Functions ==========================================================================
+
+
+const myAPI = {
+    host: 'query.yahooapis.com',
+    path: `/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(data.city)}%2C%20${data.state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
+    method: 'GET',
+};
+
+function getRestaurantsByMeal(mealType) {
+    const list = [];
+    for (let i = 0; i < data.restaurants.length; i += 1) {
+        if (data.restaurants[i].meals.search(mealType) > -1) {
+            list.push(data.restaurants[i]);
+        }
+    }
+    return list;
+}
+
+function getRestaurantByName(restaurantName) {
+    let restaurant = {};
+    for (let i = 0; i < data.restaurants.length; i += 1) {
+        if (data.restaurants[i].name === restaurantName) {
+            restaurant = data.restaurants[i];
+        }
+    }
+    return restaurant;
+}
+
+function getAttractionsByDistance(maxDistance) {
+    const list = [];
+
+    for (let i = 0; i < data.attractions.length; i += 1) {
+        if (parseInt(data.attractions[i].distance, 10) <= maxDistance) {
+            list.push(data.attractions[i]);
+        }
+    }
+    return list;
+}
+
+function getWeather(callback) {
+    const req = https.request(myAPI, (res) => {
+        res.setEncoding('utf8');
+        let returnData = '';
+
+        res.on('data', (chunk) => {
+            returnData += chunk;
+        });
+        res.on('end', () => {
+            const channelObj = JSON.parse(returnData).query.results.channel;
+
+            let localTime = channelObj.lastBuildDate.toString();
+            localTime = localTime.substring(17, 25).trim();
+
+            const currentTemp = channelObj.item.condition.temp;
+
+            const currentCondition = channelObj.item.condition.text;
+
+            callback(localTime, currentTemp, currentCondition);
+        });
+    });
+    req.end();
+}
+
+function randomArrayElement(array) {
+    let i = 0;
+    i = Math.floor(Math.random() * array.length);
+    return (array[i]);
+}
+
+const LocalizationInterceptor = {
+    process(handlerInput) {
+        const localizationClient = i18n.use(sprintf).init({
+            lng: handlerInput.requestEnvelope.request.locale,
+            overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+            resources: languageStrings,
+            returnObjects: true,
+        });
+
+        const attributes = handlerInput.attributesManager.getRequestAttributes();
+        attributes.t = function (...args) {
+            return localizationClient.t(...args);
+        };
+    },
+};
+
+// 4. Export =====================================================================================
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchHandler,
+        AboutHandler,
+        CoffeeHandler,
+        BreakfastHandler,
+        LunchHandler,
+        DinnerHandler,
+        YesHandler,
+        AttractionHandler,
+        GoOutHandler,
+        HelpHandler,
+        StopHandler,
+        FallbackHandler,
+        SessionEndedHandler
+    )
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
